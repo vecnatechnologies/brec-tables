e96a29739d777b6bc70323b4d5e5d0940c95cef6
Matt <matt.willis@vecna.com>
2015-05-01 11:29:17 -0400

    Bower deps and gulp build

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+var gulp = require('gulp'),
+  $ = require('gulp-load-plugins')();
+
+var vendorPath = 'js/vendor/';
+
+// TO-DO fix AMD error in tableTools.js
+
+var paths = [
+  vendorPath + 'datatables/media/js/jquery.dataTables.js',
+  vendorPath + 'datatables-responsive/js/dataTables.responsive.js',
+  // vendorPath + 'datatables-tabletools/js/dataTables.tableTools.js',
+  vendorPath + 'datatables-colreorder/js/dataTables.colRedorder.js',
+  vendorPath + 'datatables-colvis/js/dataTables.colVis.js',
+  vendorPath + 'jquery-datatables-column-filter/jquery.dataTables.columnFilter.js',
+  vendorPath + 'datatables-fixedcolumns/js/dataTables.fixedColumns.js',
+  vendorPath + 'datatables-fixedheader/js/dataTables.fixedHeader.js'
+];
+
+gulp.task('bower', function(cb) {
+ return gulp.src('')
+  .pipe($.run('bower install'))
+})
+
+gulp.task('datatable', ['bower'], function() {
+  return gulp.src(paths)
+    .pipe($.concat('brecTables.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('install', ['datatable']);
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var path = require('path');
 
 module.exports = {
-  sass: [path.join(__dirname, 'scss/**/*.scss')],
+  sass: [path.join(__dirname, 'scss')],
   js: [path.join(__dirname, 'js/**/*.js')]
 }
